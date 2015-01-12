5b7286db50bd14d34c75a3312302b0edcc038520
Stardos <mehdi.djenanne@gmail.com>
2015-01-12 20:54:51 +0100

    Update pion.h

--- pion.h
+++ pion.h
@@ -1,5 +1,26 @@
 #ifndef __PION_H__
 #define __PION_H__
+			////////////////////////////////////////
+			// 	   Structure de pions         //
+			////////////////////////////////////////
+	typedef struct{
+		int x; // On définit un pion par sa coordonnée en x.
+		int y; // Et par sa coordonnée en y.
+	} pion, PtSurpion;
+	
+			////////////////////////////////////////
+			//  Structure d'un ensemble de pions  //
+			////////////////////////////////////////
+
+	typedef struct{
+	
+		char couleur; // Un ensemble de pion est défini par sa couleur.
+		pion pos_pions[10]; // Pour chaque couleur un joueur a 10 pions.
+		pion label[2][10]; // Chaque couleur ne peut circuler que dans deux branches.
+		pion coup_possible[10][6]; // A chaque tour un joueur à un ensemble de coup possible pour une couleur (au plus 6*10).
+		pion sommet; // Chaque couleur à un sommet à atteindre.
+
+	} Groupe_pions, PtSurGroupe_pions;
 
 	extern Groupe_pions Groupe[6];
 	extern  char couleurs[6];
